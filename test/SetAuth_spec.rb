a3ec0d96d778dd7f3a811a664e38aaa7f039c159
Gabriele Mohr <gs@suse.de>
2014-08-28 12:51:00 +0200

    tests added for LogExecCmd and SetAuth

---
+++ test/SetAuth_spec.rb
@@ -0,0 +1,34 @@
+#! /usr/bin/rspec
+require_relative '../src/modules/IscsiLioData'
+
+describe Yast::IscsiLioDataClass do
+
+  before :each do
+    @iscsilib = Yast::IscsiLioDataClass.new
+    @iscsilib.main()
+
+    @test_class = @iscsilib
+  end
+
+  describe "#SetAuth" do
+    context "when called with user and password info" do
+      it "filters out sensitive data" do
+        tgt = ""
+        tpg = -42
+        clnt = ""
+        inc = ["SECRET1"]
+        out = ["SECRET2"]
+        expect(Yast::Builtins).to receive(:y2milestone) do |*args|
+          expect(args.to_s).not_to match /SECRET/
+        end.at_least(2).times
+
+        expect(@iscsilib).
+          to receive(:LogExecCmd).
+          twice.
+          and_return true
+
+        expect(@iscsilib.SetAuth(tgt, tpg, clnt, inc, out)).to be true
+      end
+    end
+  end
+end
